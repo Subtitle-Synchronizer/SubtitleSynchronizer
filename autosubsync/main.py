56a9d7cbe787ec93c96e92c686d755a9baded64e
Abhishek <618757@QEAQI.COGNIZANT.COM>
2020-05-19 13:21:48 -0500

    Committng code for autosubsync

---
+++ autosubsync/main.py
@@ -0,0 +1,188 @@
+#!/usr/bin/python3
+import argparse
+import numpy as np
+import gc
+import os
+import sys
+
+def parse_skew(skew):
+    "helper function, parse maybe fractional notation like 24/24 to float"
+    if skew is None: return None
+    if '/' in skew:
+        a, b = [float(x) for x in skew.split('/')]
+        return a / b
+    else:
+        return float(skew)
+
+def synchronize(video_file, subtitle_file, output_file, verbose=False, \
+    parallelism=3, fixed_skew=None, model_file=None, return_parameters=False, \
+    **kwargs):
+    """
+    Automatically synchronize subtitles with audio in a video file.
+    Uses FFMPEG to extract the audio from the video file and the command line
+    tool "ffmpeg" must be available. Uses temporary files which are deleted
+    automatically.
+
+    Args:
+        video_file (string): Input video file name
+        subtitle_file (string): Input SRT subtitle file name
+        output_file (string): Output (synchronized) SRT subtitle file name
+        verbose (boolean): If True, print progress information to stdout
+        return_parameters (boolean): If True, returns the syncrhonization
+            parameters instead of just the success flag
+        other arguments: Search parameters, see ``autosubsync --help``
+
+    Returns:
+        If return_parameters is False (default), returns
+        True on success (quality of fit test passed), False if failed.
+
+        If return_parameters is True, returns a tuple of four values
+
+            success (boolean)   success flag as above
+            quality (float)     metric used to determine the value of "success"
+            skew (float)        best fit skew/speed (unitless)
+            shift (float)       best fit shift in seconds
+
+    """
+
+    # these are here to enable running as python3 autosubsync/main.py
+    from autosubsync import features
+    from autosubsync import find_transform
+    from autosubsync import model
+    from autosubsync import preprocessing
+    from autosubsync import quality_of_fit
+    from autosubsync import srt_io
+
+    # first check that the SRT file is valid before extracting any audio data
+    srt_io.check_file(subtitle_file)
+
+    # argument parsing
+    if model_file is None:
+        from pkg_resources import resource_filename
+        model_file = resource_filename(__name__, '../trained-model.bin')
+
+    fixed_skew = parse_skew(fixed_skew)
+    # load model
+    print('model fie: ', model_file)
+    trained_model = model.load(model_file)
+    print('bias: ', trained_model[1])
+
+    if verbose: print('Extracting audio using ffmpeg and reading subtitles...')
+    sound_data, subvec = preprocessing.import_target_files(video_file, subtitle_file)
+
+    if verbose: print(('computing features for %d audio samples ' + \
+        'using %d parallel process(es)') % (len(subvec), parallelism))
+
+    features_x, shifted_y = features.compute(sound_data, subvec, parallelism=parallelism)
+    np.save('features_file_X.npy', features_x)
+    np.save('features_file_Y.npy', shifted_y)
+
+    if verbose: print('extracted features of size %s, performing speech detection' % \
+        str(features_x.shape))
+
+    y_scores = model.predict(trained_model, features_x)
+
+    y_converted_scores = []
+    for score in y_scores:
+        if score > 0.5:
+            y_converted_scores.append(1)
+        else:
+            y_converted_scores.append(0)
+
+    import sklearn.metrics as metrics
+    from sklearn.metrics import accuracy_score
+    from sklearn.metrics import f1_score
+    from sklearn.metrics import precision_score
+    from sklearn.metrics import recall_score
+
+    classification_report = metrics.classification_report(shifted_y, y_converted_scores)
+    print("Classification report: \n", classification_report)
+    confusion_matrix = metrics.confusion_matrix(shifted_y, y_converted_scores)
+    print("Confusion matrix: \n", confusion_matrix)
+
+    print("Accuracy Score: ", accuracy_score(shifted_y, y_converted_scores))
+    print("F1 Score: ", f1_score(shifted_y, y_converted_scores, average='weighted'))
+    print("Precision Score: ", precision_score(shifted_y, y_converted_scores, average='weighted'))
+    print("Recall Score: ", recall_score(shifted_y, y_converted_scores, average='weighted'))
+
+    # save some memory before parallelization fork so we look less bad
+    del features_x, sound_data, subvec
+    gc.collect()
+
+    if verbose:
+        print('computing best fit with %d frames' % len(y_scores))
+
+    skew, shift, quality = find_transform.find_transform_parameters(\
+        shifted_y, y_scores, \
+        parallelism=parallelism, fixed_skew=fixed_skew, bias=trained_model[1], \
+        verbose=verbose, **kwargs)
+
+    success = quality > quality_of_fit.threshold
+    if verbose:
+        print('quality of fit: %g, threshold %g' % (quality, quality_of_fit.threshold))
+        print('Fit complete. Performing resync, writing to ' + output_file)
+
+    transform_func = find_transform.parameters_to_transform(skew, shift)
+    preprocessing.transform_srt(subtitle_file, output_file, transform_func)
+
+    if verbose and success: print('success!')
+
+    if return_parameters:
+        return success, quality, skew, shift
+    else:
+        return success
+
+
+def cli(packaged_model=False):
+    p = argparse.ArgumentParser(description=synchronize.__doc__.split('\n\n')[0])
+
+    #p.add_argument('video_file', default='Deadpool2.mp4', help='Input video file')
+    #p.add_argument('subtitle_file', default='Deadpool2.srt', help='Input SRT subtitle file')
+    #p.add_argument('output_file', default='Deadpool2_Output.srt', help='Output (auto-synchronized) SRT subtitle file')
+
+    # Make model file an argument only in the non-packaged version
+    print('packaged_model: ', packaged_model)
+    if not packaged_model:
+        p.add_argument('--model_file', default='trained-model.bin')
+
+    p.add_argument('--max_shift_secs', default=20.0, type=float,
+        help='Maximum subtitle shift in seconds (default 20)')
+    p.add_argument('--parallelism', default=3, type=int,
+        help='Number of parallel worker processes (default 3)')
+    p.add_argument('--fixed_skew', default=None,
+        help='Use a fixed skew (e.g. 1) instead of auto-detection')
+    p.add_argument('--silent', action='store_true',
+        help='Do not print progress information')
+    args = p.parse_args()
+
+
+    if packaged_model:
+        model_file = None
+    else:
+        model_file = args.model_file
+
+    subtitle_file='Deadpool2.srt'
+    output_file = 'Deadpool2_Output.srt'
+
+    video_file = 'GardenofEvil.mp4'
+    subtitle_file = 'GardenofEvil_10_2.srt'
+    output_file = 'GardenofEvil_10_2_Output_autosubsync_with_librosa.srt'
+    success = synchronize(video_file, subtitle_file, output_file,\
+                  verbose = not args.silent,
+                  model_file = model_file,
+                  max_shift_secs = args.max_shift_secs, \
+                  parallelism = args.parallelism,
+                  fixed_skew = args.fixed_skew)
+
+    if not success:
+        sys.stderr.write("\nWARNING: low quality of fit. Wrong subtitle file?\n")
+        sys.exit(1)
+
+def cli_packaged():
+    """Entry point in the packaged, pip-installable version"""
+    cli(packaged_model=True)
+
+if __name__ == '__main__':
+    # Entry point for running from repository root folder
+    sys.path.append('.')
+    cli(packaged_model=False)
---
+++ autosubsync/preprocessing.py
@@ -0,0 +1,142 @@
+import os
+import sys
+import tempfile
+import subprocess
+import numpy as np
+from . import srt_io
+
+def import_sound(sound_path):
+    import soundfile
+
+    target_sample_rate = 20000
+    warning_threshold = 8000
+
+    # minimize memory usage by reading audio as 16-bit integers instead of
+    # float or double. This should be the maximum precision of the samples
+    # anyway
+    samples, sample_rate = soundfile.read(sound_path, dtype='int16')
+    data_range = 2**15
+
+    # even 8-bit audio would work here, but already affects performance
+    # samples, data_range = (samples / 256).astype(np.int8), 128
+
+    ds_factor = max(int(np.floor(sample_rate / target_sample_rate)), 1)
+    samples = samples[::ds_factor]
+    sample_rate = sample_rate / ds_factor
+    if sample_rate < warning_threshold:
+        # probably never happens but checking anyway
+        sys.stderr.write('warning: low sound sample rate %d Hz\n' % sample_rate)
+
+    return samples, sample_rate, data_range
+
+
+def import_sound_test(sound_path):
+    import soundfile
+
+    target_sample_rate = 20000
+    warning_threshold = 8000
+
+    # minimize memory usage by reading audio as 16-bit integers instead of
+    # float or double. This should be the maximum precision of the samples
+    # anyway
+
+    original_samples, sample_rate = soundfile.read(sound_path, dtype='int16')
+    data_range = 2**15
+    samples = []
+    for i in range(len(original_samples)):
+        samples.append(np.mean(original_samples[i]))
+    # even 8-bit audio would work here, but already affects performance
+    # samples, data_range = (samples / 256).astype(np.int8), 128
+
+    ds_factor = max(int(np.floor(sample_rate / target_sample_rate)), 1)
+    samples = samples[::ds_factor]
+    sample_rate = sample_rate / ds_factor
+    if sample_rate < warning_threshold:
+        # probably never happens but checking anyway
+        sys.stderr.write('warning: low sound sample rate %d Hz\n' % sample_rate)
+
+    return samples, sample_rate, data_range
+
+
+def build_sub_vec(subs, sample_rate, n, sub_filter=None):
+    subvec = np.zeros(n, np.bool)
+    #subvec = np.zeros(n)
+    to_index = lambda x: int(sample_rate*x)
+    for line in subs:
+        if sub_filter is not None and not sub_filter(line.text): continue
+        subvec[to_index(line.begin):to_index(line.end)] = 1
+    return subvec
+
+def import_subs(srt_filename, sample_rate, n, **kwargs):
+    audio_length = n / float(sample_rate)
+    subs = list(srt_io.read_file(srt_filename))
+    if len(subs) > 0:
+        subs_length = np.max([s.end for s in subs])
+        rel_err = abs(subs_length - audio_length) / max(subs_length, audio_length)
+        if rel_err > 0.25: # warning threshold
+            sys.stderr.write(" *** WARNING: subtitle and audio lengths " + \
+                             "differ by %d%%. Wrong subtitle file?\n" % int(rel_err*100))
+
+    else:
+        sys.stderr.write(" *** WARNING: empty subtitle file\n")
+    return build_sub_vec(subs, sample_rate, n, **kwargs)
+
+def import_item(sound_file, subtitle_file, **kwargs):
+    sound_data = import_sound_test(sound_file)
+    samples, sample_rate, data_range = sound_data
+    n = len(samples)
+    print('audio duration: ', n/sample_rate)
+    sub_vec = import_subs(subtitle_file, sample_rate, n, **kwargs)
+    return sound_data, sub_vec
+
+
+def extract_sound(input_video_file, output_sound_file):
+    convert_cmd = [
+        'ffmpeg',
+        '-y', # overwrite if exists
+        '-loglevel', 'error',
+        '-i', input_video_file, # input
+        '-ac', '1', # convert to mono
+        output_sound_file
+    ]
+    subprocess.call(convert_cmd)
+
+
+def extractVocalSignals(input_audio_file, output_vocal_dir):
+    extract_sound_cmd = [
+         'spleeter',
+         'separate',  # overwrite if exists
+         '-i', input_audio_file,  # input
+         '-p', 'spleeter:2stems',  # convert to mono
+        '-d', '600',
+        '-o', output_vocal_dir
+     ]
+    output = subprocess.call(extract_sound_cmd)
+    print('output: ', output)
+
+def import_target_files(video_file, subtitle_file, **kwargs):
+    "Import prediction target files using a temporary directory"
+    tmp_dir = tempfile.mkdtemp()
+    sound_file = os.path.join(tmp_dir, 'sound.flac')
+
+    def clear():
+        try: os.unlink(sound_file)
+        except: pass
+
+        try: os.rmdir(tmp_dir)
+        except: pass
+
+    try:
+        extract_sound(video_file, sound_file)
+        extractVocalSignals(sound_file, tmp_dir)
+        vocal_file = os.path.join(tmp_dir, 'sound', 'vocals.wav')
+        return import_item(vocal_file, subtitle_file, **kwargs)
+
+    finally:
+        clear()
+
+def transform_srt(in_srt, out_srt, transform_func):
+    with open(out_srt, 'wb') as out_file:
+        out_srt = srt_io.writer(out_file)
+        for sub in srt_io.read_file(in_srt):
+            out_srt.write(transform_func(sub.begin), transform_func(sub.end), sub.text)
